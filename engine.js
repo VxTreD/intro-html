fa4e772e1dabd942492bf5793d8657745873f1a1
vxtred <viniciusnano@hotmail.com>
2023-05-19 01:22:26 -0300

    introduzindo js basico

---
+++ engine.js
@@ -0,0 +1,10 @@
+function butao() {
+  var texto = "Texto do alerta";
+  alert("Seu mouse está ok :)");
+}
+
+var botao = document.getElementById('butao');
+
+botao.addEventListener('click', butao);
+
+  
